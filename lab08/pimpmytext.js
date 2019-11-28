a0dceaa4e480c5d530074b49cc597e76df342292
ggjae <ggjae_@daum.net>
2019-11-28 17:25:11 +0900

    lab09

--- lab08/pimpmytext.js
+++ lab08/pimpmytext.js
@@ -1,39 +1,100 @@
-function onclick_event(){
-    var text = document.getElementById("text");
-    text.innerHTML = "Here is the sample text. Let's see if the 'Bigger Pimpin'!' button works!";
-    bigtime();
+function hello() {
+    alert("Hello, World!");
+  }
+  
+  // function modifyArea1() {
+  //   var textarea = document.getElementById("area1");
+  //   textarea.style.fontSize = "24pt";
+  // }
+function pageLoad() {
+    document.getElementById("pimp").onclick = timer;
+    document.getElementById("check1").onclick = bling;
+    document.getElementById("snoopify").onclick = snoopify;
+    document.getElementById("igpay").onclick = igpay;
+    document.getElementById("malkovitch").onclick = malkovitch;
 }
-function bigger() {
-    var size = parseInt(text.style.fontSize) + 2;
-    text.style.fontSize = size + "pt";
-}
-
-function bigtime() {
-
-    text.style.fontSize = "12pt";
-    setInterval("bigger()", 500);
-}
-function change(){
-    var text1 = document.getElementById("text");
-    var text2 = document.getElementById("Bling");
-    console.log(text2.checked);
-    if(text2.checked == true){
-        text1.style.fontWeight = "Bold";
-        text1.style.color = "green";
-        text1.style.textDecoration = "underline";
+  
+  function bling() {
+    var textarea = document.getElementById("area1");
+    var checkbox = document.getElementById("check1");
+    if (checkbox.checked == true) {
+      textarea.style.fontWeight = "bold";
+      textarea.style.color = "green";
+      textarea.style.textDecoration = "underline";
+      document.body.style.backgroundImage =
+        "url(https://selab.hanyang.ac.kr/courses/cse326/2019/labs/images/8/hundred.jpg)";
     } else {
-        text1.style.fontWeight = "";
-        text1.style.color = "";
-        text1.style.textDecoration = "";
+      textarea.style.fontWeight = "normal";
+      textarea.style.color = "black";
+      textarea.style.textDecoration = "none";
+      document.body.style.backgroundImage = "none";
     }
-    
-}
-function snoof(){
-    var text = document.getElementById("text");
-    text.innerHTML = "Here is the sample text. Let's see if the 'Bigger Pimpin'!' button works!";
-    var text2 = text.innerHTML.toUpperCase();
-    var a = text2.split(".");
-    text.innerHTML = a.join("-izzle.")
-}
-
-
+  }
+  
+  function snoopify() {
+    var textarea = document.getElementById("area1");
+    textarea.value = textarea.value.toUpperCase();
+    textarea.value = textarea.value.split(".").join("-izzle");
+  }
+  
+  function modifyArea1() {
+    var textarea = document.getElementById("area1");
+    if (textarea.style.fontSize == false) {
+      textarea.style.fontSize = "12pt";
+    } else {
+      textarea.style.fontSize = parseInt(textarea.style.fontSize) + 2 + "pt";
+    }
+  }
+  
+  function timer() {
+    setInterval(modifyArea1, 500);
+  }
+  
+  function pigLatin(str) {
+    var textarea = document.getElementById("area1");
+    str = textarea.value;
+    str = str.toLowerCase();
+    const vowels = ["a", "e", "i", "o", "u"];
+    let vowelIndex = 0;
+    if (vowels.includes(str[0])) {
+      return str + "ay";
+    } else {
+      for (let char of str) {
+        if (vowels.includes(char)) {
+          vowelIndex = str.indexOf(char);
+          break;
+        }
+      }
+      return str.slice(vowelIndex) + str.slice(0, vowelIndex) + "ay";
+    }
+  }
+  
+  function igpay() {
+    var textarea = document.getElementById("area1");
+    var str = textarea.value;
+    var result = str;
+    var i, j;
+    for (i = 0; i < str.length; i++) {
+      for (j = i; j < str.length; j++);
+      result = result.replace(str.slice(i, j), pigLatin(str.slice(i, j)));
+      i = j;
+    }
+    textarea.value = result;
+  }
+  
+  function malkovitch() {
+    var textarea = document.getElementById("area1");
+    var str = textarea.value;
+    var result = str;
+    var i, j;
+    for (i = 0; i < str.length; i++) {
+      for (j = i; j < str.length; j++) {
+        if (j - i >= 5) {
+          result = result.replace(str.slice(i, j), "Malkovitch");
+        }
+      }
+      i = j;
+    }
+    textarea.value = result;
+  }
+window.onload = pageLoad;
---
+++ lab09/maze.js
@@ -0,0 +1,63 @@
+/* CSE3026 : Web Application Development
+ * Lab 09 - Maze
+ */
+"use strict";
+
+var loser = null;  // whether the user has hit a wall
+
+window.onload = function() {
+    var bound = $$(".boundary");
+    for (var i = 0; i < bound.length; i++){
+        bound[i].onmouseover = overBoundary;
+    }
+    $("end").onmouseover = overEnd;
+    $("start").observe("click", startClick);
+    $("maze").observe("mouseout", overBody);
+    $("maze").observe("mousemove", overBody);
+};
+
+// called when mouse enters the walls; 
+// signals the end of the game with a loss
+function overBoundary(event) {
+    var bound = $$(".boundary");
+    for (var i = 0; i < bound.length; i++){
+        bound[i].addClassName("youlose");
+    }
+    loser = true;
+    overEnd;
+    
+}
+
+// called when mouse is clicked on Start div;
+// sets the maze back to its initial playable state
+function startClick() {
+    var bound = $$(".boundary");
+    for (var i = 0; i < bound.length; i++){
+        bound[i].removeClassName("youlose");
+    }
+    $("status").innerHTML = "Maze Start!";
+    loser = false;
+}
+
+// called when mouse is on top of the End div.
+// signals the end of the game with a win 수정 필요..19/11/28
+function overEnd() {
+    if (loser) {
+        $("status").innerHTML = "You lose! :( ";
+    } else {
+        $("status").innerHTML = "You Win! :)";
+    }
+}
+
+// test for mouse being over document.body so that the player
+// can't cheat by going outside the maze
+function overBody(event) {
+    if (loser === false){
+        if(event.target.getAttribute("id") === null){
+            overBoundary();
+        }
+    }
+}
+
+
+
