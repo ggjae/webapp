44ae92fa1631a8f430361de576b4b06ad58f5397
ggjae <ggjae_@daum.net>
2019-12-10 03:57:33 +0900

    lab10 modify

--- lab10/book_search.js
+++ lab10/book_search.js
@@ -1,36 +1,79 @@
-window.onload = function() {
-    $("b_xml").onclick=function(){
-    	    //construct a Prototype Ajax.request object
-    }
-    $("b_json").onclick=function(){
-    	    //construct a Prototype Ajax.request object
-    }
-};
-
-function getCheckedRadio(radio_button){
+window.onload = function () {
+	$("b_xml").onclick = function () {
+	   new Ajax.Request("books.php", {
+		  method: "get",
+		  parameters: { category: getCheckedRadio($$("#category input")) },
+		  onSuccess: showBooks_XML,
+		  onFailure: ajaxFailed,
+		  onException: ajaxFailed
+	   });
+	};
+	$("b_json").onclick = function () {
+	   new Ajax.Request("books_json.php", {
+		  method: "get",
+		  parameters: { category: getCheckedRadio($$("#category input")) },
+		  onSuccess: showBooks_JSON,
+		  onFailure: ajaxFailed,
+		  onException: ajaxFailed
+	   });
+	};
+ };
+ 
+ function getCheckedRadio(radio_button) {
 	for (var i = 0; i < radio_button.length; i++) {
-		if(radio_button[i].checked){
-			return radio_button[i].value;
-		}
+	   if (radio_button[i].checked) {
+		  return radio_button[i].value;
+	   }
 	}
 	return undefined;
-}
-
-function showBooks_XML(ajax) {
-	alert(ajax.responseText);
-}
-
-function showBooks_JSON(ajax) {
-	alert(ajax.responseText);
-}
-
-function ajaxFailed(ajax, exception) {
+ }
+ 
+ function showBooks_XML(ajax) {
+	var xmlDom = ajax.responseXML;
+	var elms = xmlDom.getElementsByTagName("book");
+	var output = "";
+	for (var i = 0; i < elms.length; i++) {
+	   output +=
+		  "<li>" +
+		  elms[i].getElementsByTagName("title")[0].firstChild.nodeValue +
+		  ", by" +
+		  elms[i].getElementsByTagName("author")[0].firstChild.nodeValue +
+		  "(" +
+		  elms[i].getElementsByTagName("year")[0].firstChild.nodeValue +
+		  ")</li>";
+	}
+	$("books").innerHTML = output;
+ }
+ 
+ function showBooks_JSON(ajax) {
+	var data = JSON.parse(ajax.responseText).books;
+	var output = "";
+	for (var i = 0; i < data.length; i++) {
+	   output +=
+		  "<li>" +
+		  data[i].title +
+		  ", by" +
+		  data[i].author +
+		  "(" +
+		  data[i].year +
+		  ")</li>";
+	}
+	$("books").innerHTML = output;
+	//   alert(ajax.responseText);
+ }
+ 
+ function ajaxFailed(ajax, exception) {
 	var errorMessage = "Error making Ajax request:\n\n";
 	if (exception) {
-		errorMessage += "Exception: " + exception.message;
+	   errorMessage += "Exception: " + exception.message;
 	} else {
-		errorMessage += "Server status:\n" + ajax.status + " " + ajax.statusText + 
-		                "\n\nServer response text:\n" + ajax.responseText;
+	   errorMessage +=
+		  "Server status:\n" +
+		  ajax.status +
+		  " " +
+		  ajax.statusText +
+		  "\n\nServer response text:\n" +
+		  ajax.responseText;
 	}
 	alert(errorMessage);
-}
+ }
