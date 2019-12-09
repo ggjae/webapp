81219d8967100cc30e992439a6a8ecbe916cb214
ggjae <ggjae_@daum.net>
2019-12-10 04:06:49 +0900

    lab09 modify

--- lab09/maze.js
+++ lab09/maze.js
@@ -1,63 +1,60 @@
-/* CSE3026 : Web Application Development
- * Lab 09 - Maze
- */
 "use strict";
 
-var loser = null;  // whether the user has hit a wall
+var loser = null; // whether the user has hit a wall
+var won = false;
 
 window.onload = function() {
-    var bound = $$(".boundary");
-    for (var i = 0; i < bound.length; i++){
-        bound[i].onmouseover = overBoundary;
-    }
-    $("end").onmouseover = overEnd;
-    $("start").onclick = startClick;
-    $("maze").onmouseout = overBody;
-    document.body.observe("mousemove", overBody);
+  var divs = $$(".boundary");
+  for (var i = 0; i < divs.length; i++) {
+    divs[i].onmouseover = overBoundary;
+  }
+  $("end").onmouseover = overEnd;
+  $("start").onclick = startClick;
+  $("maze").onmouseout = overBody;
+  document.body.observe("mousemove", overBody);
 };
 
-// called when mouse enters the walls; 
+// called when mouse enters the walls;
 // signals the end of the game with a loss
 function overBoundary(event) {
-    var bound = $$(".boundary");
-    for (var i = 0; i < bound.length; i++){
-        bound[i].addClassName("youlose");
-    }
-    loser = true;
-    overEnd;
-    
+  var divs = $$(".boundary");
+  for (var i = 0; i < divs.length; i++) {
+    divs[i].addClassName("youlose");
+  }
+  loser = true;
+  overEnd();
 }
 
 // called when mouse is clicked on Start div;
 // sets the maze back to its initial playable state
 function startClick() {
-    var bound = $$(".boundary");
-    for (var i = 0; i < bound.length; i++){
-        bound[i].removeClassName("youlose");
-    }
-    $("status").innerHTML = "Maze Start!";
-    loser = false;
+  var divs = $$(".boundary");
+  for (var i = 0; i < divs.length; i++) {
+    divs[i].removeClassName("youlose");
+  }
+  $("status").innerHTML = "Maze Start!";
+  loser = false;
+  won = false;
 }
 
 // called when mouse is on top of the End div.
-// signals the end of the game with a win 수정 필요..19/11/28
+// signals the end of the game with a win
 function overEnd() {
-    if (loser) {
-        $("status").innerHTML = "You lose! :( ";
-    } else {
-        $("status").innerHTML = "You Win! :)";
-    }
+  if (loser) {
+    $("status").innerHTML = "You lose! :( ";
+  } else {
+    $("status").innerHTML = "You Win! :)";
+    won = true;
+    console.log(won);
+  }
 }
 
 // test for mouse being over document.body so that the player
 // can't cheat by going outside the maze
 function overBody(event) {
-    if (loser === false){
-        if(event.target.getAttribute("id") === null){
-            overBoundary();
-        }
+  if (loser === false) {
+    if (event.target.getAttribute("id") === null) {
+      overBoundary();
     }
+  }
 }
-
-
-
